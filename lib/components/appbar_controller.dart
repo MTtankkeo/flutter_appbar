d8f8cc0a57b377669f3ceea9fd46dd88a79ed7b1
Dev Ttangkong <ttankkeo112@gmail.com>
2024-10-04 01:22:07 +0900

    commit for removing import

--- lib/components/appbar_controller.dart
+++ lib/components/appbar_controller.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_appbar/components/appbar_position.dart';
